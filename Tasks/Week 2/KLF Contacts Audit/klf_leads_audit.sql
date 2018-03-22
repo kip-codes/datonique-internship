90a4f91c48102df17645db578f88abd0067e89d8
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-03-22 14:33:36 -0700

    more contact queries

--- Tasks/Week 2/KLF Contacts Audit/klf_leads_audit.sql
+++ Tasks/Week 2/KLF Contacts Audit/klf_leads_audit.sql
@@ -9,7 +9,9 @@ LIMIT 2;
 SELECT *
 FROM klf.contacts
 WHERE phone1 IS NOT NULL AND address_billing_postal_code IS NOT NULL
-LIMIT 15;
+  AND EXTRACT(year FROM date_created) = 2018 and EXTRACT(month from date_created) = 3
+      AND (EXTRACT(day FROM date_created) >= 5 AND EXTRACT(day from date_created) <= 11)
+LIMIT 25;
 
 /* 1/1/18 - 1/7/18 */
 CREATE TABLE kevin_ip.lead_source_report_staging AS (
@@ -20,18 +22,18 @@ CREATE TABLE kevin_ip.lead_source_report_staging AS (
       END AS "Name"
       , regexp_replace(phone1, '[^0-9]+', '') AS "Phone 1"
       , address_billing_postal_code AS "Postal Code"
-      , NULL AS "DateCreatedPT"
-      , NULL AS "Owner"
-      , NULL AS "Leadsource"
-      , NULL AS "Lead Source Category"
-      , NULL AS "Lead Mode"
-      , NULL AS "Search Engine Referrer"
-      , NULL AS "Source / Website"
-      , NULL AS "Website/Landing Page"
-      , NULL AS "Search Engine"
-      , NULL AS "Lead ID"
-      , NULL AS "Lead Source Code"
-      , NULL AS "ID"
+      , date_created AS "DateCreatedPT"
+      , salesperson AS "Owner" -- Point of Sales contact
+      , NULL AS "Leadsource" -- FIXME
+      , NULL AS "Lead Source Category" -- FIXME
+      , lead_mode AS "Lead Mode"
+      , NULL AS "Search Engine Referrer" -- FIXME
+      , "source_/_website" AS "Source / Website"
+      , NULL AS "Website/Landing Page" -- FIXME
+      , NULL AS "Search Engine" -- FIXME
+      , NULL AS "Lead ID" -- FIXME
+      , NULL AS "New Lead Source" -- FIXME
+      , id AS "ID"
       , NULL AS "Date Retained"
       , NULL AS "Retainer Amount"
       , NULL AS "Downpay"
@@ -56,6 +58,7 @@ CREATE TABLE kevin_ip.lead_source_report_staging AS (
       , NULL AS "What Station of Website"
 
     FROM klf.contacts
+    -- Custom date range
     WHERE EXTRACT(year FROM date_created) = 2018 and EXTRACT(month from date_created) = 1
       AND (EXTRACT(day FROM date_created) >= 1 AND EXTRACT(day from date_created) <= 7)
 );
@@ -84,3 +87,61 @@ SELECT
   address_billing_postal_code as zip
 FROM klf.contacts
 LIMIT 50;
+
+
+-- Date created PT
+SELECT
+  date_created
+FROM klf.contacts
+LIMIT 15;
+-- accept raw timestamp
+
+
+-- Owner / Sales person
+SELECT salesperson
+FROM klf.contacts
+LIMIT 15;
+
+
+-- Lead Source Category
+-- UNAVAILABLE
+
+
+-- Lead Mode
+SELECT lead_mode
+from klf.contacts
+LIMIT 15;
+
+
+-- Search Engine Referrer
+-- UNAVAILABLE
+
+
+-- Source / Website"
+-- UNAVAILABLE
+
+
+-- "Website/Landing Page"
+-- UNAVAILABLE
+
+
+-- "Search Engine"
+-- UNAVAILALE
+
+
+-- "Lead ID"
+-- UNAVAILABLE
+
+
+-- "New Lead Source"
+-- UNAVAILABLE
+
+
+-- ID
+SELECT id
+FROM klf.contacts
+LIMIT 15;
+-- Unique identifier per contact
+-- unsure if PK
+
+
