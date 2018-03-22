a871077a5a5251b4484d44c36cadaaea47eeb2b5
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-03-22 13:37:29 -0700

    started klf

--- Tasks/Week 2/KLF Contacts Audit/klf_leads_audit.sql
+++ Tasks/Week 2/KLF Contacts Audit/klf_leads_audit.sql
@@ -5,6 +5,12 @@ SELECT extract(year from date_created), extract(month from date_created), date_c
 FROM klf.contacts
 LIMIT 2;
 
+
+SELECT *
+FROM klf.contacts
+WHERE phone1 IS NOT NULL AND address_billing_postal_code IS NOT NULL
+LIMIT 15;
+
 /* 1/1/18 - 1/7/18 */
 CREATE TABLE kevin_ip.lead_source_report_staging AS (
     SELECT
@@ -12,8 +18,8 @@ CREATE TABLE kevin_ip.lead_source_report_staging AS (
         WHEN family_name IS NOT NULL THEN TRIM(BOTH FROM given_name) + ' ' + TRIM(BOTH FROM family_name)
         ELSE TRIM(BOTH FROM given_name)
       END AS "Name"
-      , NULL AS "Phone 1"
-      , NULL AS "Postal Code"
+      , regexp_replace(phone1, '[^0-9]+', '') AS "Phone 1"
+      , address_billing_postal_code AS "Postal Code"
       , NULL AS "DateCreatedPT"
       , NULL AS "Owner"
       , NULL AS "Leadsource"
@@ -54,6 +60,8 @@ CREATE TABLE kevin_ip.lead_source_report_staging AS (
       AND (EXTRACT(day FROM date_created) >= 1 AND EXTRACT(day from date_created) <= 7)
 );
 
+
+-- Name
 SELECT
   CASE
     WHEN family_name IS NOT NULL THEN TRIM(BOTH FROM given_name) + ' ' + TRIM(BOTH FROM family_name)
@@ -61,3 +69,18 @@ SELECT
   END AS "Name"
 FROM klf.contacts
 LIMIT 15;
+
+
+-- Phone 1
+SELECT
+  regexp_replace(phone1, '[^0-9]+', '') as "Phone 1"
+FROM klf.contacts
+LIMIT 15;
+-- Cannot guarantee format of phone number (international vs local), so will not truncate
+
+
+-- Zip code
+SELECT
+  address_billing_postal_code as zip
+FROM klf.contacts
+LIMIT 50;
