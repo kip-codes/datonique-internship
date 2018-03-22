de0c00d61eb0556b06406dcf32c6a101f72ef58a
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-03-22 11:25:23 -0700

    renamed variables

--- Tasks/Week 2/KLF Contacts Audit/klf_leads_audit.sql
+++ Tasks/Week 2/KLF Contacts Audit/klf_leads_audit.sql
@@ -1,35 +1,63 @@
 /*
 Sample rows of data
  */
-SELECT extract(year from date_created), extract(month from date_created)
+SELECT extract(year from date_created), extract(month from date_created), date_created
 FROM klf.contacts
 LIMIT 2;
 
-CREATE VIEW kevin_ip.lead_source_report AS (
-    SELECT *
+/* 1/1/18 - 1/7/18 */
+CREATE TABLE kevin_ip.lead_source_report_staging AS (
+    SELECT
+      CASE
+        WHEN family_name IS NOT NULL THEN TRIM(BOTH FROM given_name) + ' ' + TRIM(BOTH FROM family_name)
+        ELSE TRIM(BOTH FROM given_name)
+      END AS "Name"
+      , NULL AS "Phone 1"
+      , NULL AS "Postal Code"
+      , NULL AS "DateCreatedPT"
+      , NULL AS "Owner"
+      , NULL AS "Leadsource"
+      , NULL AS "Lead Source Category"
+      , NULL AS "Lead Mode"
+      , NULL AS "Search Engine Referrer"
+      , NULL AS "Source / Website"
+      , NULL AS "Website/Landing Page"
+      , NULL AS "Search Engine"
+      , NULL AS "Lead ID"
+      , NULL AS "Lead Source Code"
+      , NULL AS "ID"
+      , NULL AS "Date Retained"
+      , NULL AS "Retainer Amount"
+      , NULL AS "Downpay"
+      , NULL AS "Type"
+      , NULL AS "DateCreated"
+      , NULL AS "Time"
+      , NULL AS "Hour"
+      , NULL AS "Month and Year"
+      , NULL AS "Year"
+      , NULL AS "Month"
+      , NULL AS "Day"
+      , NULL AS "Week"
+      , NULL AS "Weekday"
+      , NULL AS "County of Arrest"
+      , NULL AS "Court"
+      , NULL AS "TimeGroup" -- keep null
+      , NULL AS "Id" -- keep null
+      , NULL AS "Shift" -- keep null
+      , NULL AS "Offense"
+      , NULL AS "Spend" -- keep null
+      , NULL AS "Radio/TV/Internet"
+      , NULL AS "What Station of Website"
+
     FROM klf.contacts
-    WHERE EXTRACT(year FROM date_created) = 2018 and EXTRACT(month from date_created) = 3
-      AND (EXTRACT(day FROM date_created) >= 5 AND EXTRACT(day from date_created) <= 11)
+    WHERE EXTRACT(year FROM date_created) = 2018 and EXTRACT(month from date_created) = 1
+      AND (EXTRACT(day FROM date_created) >= 1 AND EXTRACT(day from date_created) <= 7)
 );
 
 SELECT
-  given_name
-  , family_name
-  , phone1
-  , SUM(retainer_amount) as retainer_amount
-FROM kevin_ip.lead_source_report
-GROUP BY family_name, phone1, retainer_amount, given_name
-ORDER BY phone1 ASC ;
-
-SELECT
-  family_name
-  , phone1
-  , retainer_amount
-  , date_created
-  , date_retained
+  CASE
+    WHEN family_name IS NOT NULL THEN TRIM(BOTH FROM given_name) + ' ' + TRIM(BOTH FROM family_name)
+    ELSE TRIM(BOTH FROM given_name)
+  END AS "Name"
 FROM klf.contacts
-WHERE retainer_amount > 0 and EXTRACT(year FROM date_retained) = 2018 and EXTRACT(month from date_retained) = 3;
-
-
-SELECT *
-FROM kevin_ip.lead_source_report
+LIMIT 15;
--- Tasks/Week 2/Maintenance Audit/training_3-21.sql
+++ Tasks/Week 2/Maintenance Audit/training_3-21.sql
@@ -1,154 +1,154 @@
 SELECT 'fanjoy_customers_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at)
+  , MAX(updated_at) as last_updated
 FROM public.fanjoy_customers_data;
 
 
 SELECT 'fanjoy_lineitems_data' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM public.fanjoy_lineitems_data;
 
 
 SELECT 'fanjoy_orders_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM public.fanjoy_orders_data;
 
 
 SELECT 'ga_event_data' as table_name
   , count(*) as num_rows
-  , MAX(date) as date
+  , MAX(date) as last_updated
 FROM public.ga_event_data;
 
 
 SELECT 'ga_pagetracking_data' as table_name
   , count(*) as num_rows
-  , MAX(date) as date
+  , MAX(date) as last_updated
 FROM public.ga_pagetracking_data;
 
 
 SELECT 'ga_session_data' as table_name
   , count(*) as num_rows
-  , MAX(date) as date
+  , MAX(date) as last_updated
 FROM public.ga_session_data;
 
 
 SELECT 'ga_transaction_data' as table_name
   , count(*) as num_rows
-  , MAX(date) as date
+  , MAX(date) as last_updated
 FROM ga_transaction_data;
 
 
 SELECT 'ga_user_data' as table_name
   , count(*) as num_rows
-  , MAX(date) as date
+  , MAX(date) as last_updated
 FROM ga_user_data;
 
 
 SELECT 'ga_user_demo_data' as table_name
   , count(*) as num_rows
-  , MAX(date) as date
+  , MAX(date) as last_updated
 FROM ga_user_demo_data;
 
 
 SELECT 'klaviyo_orders_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM klaviyo_orders_data;
 
 
 SELECT 'klaviyo_profiles_data' as table_name
   , count(*) as num_rows
-  , MAX(last_import) as date
+  , MAX(last_import) as last_updated
 FROM klaviyo_profiles_data;
 
 
 SELECT 'replyyes' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM survey.replyyes;
 
 
 SELECT 'survey_answers' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM survey.survey_answers;
 
 
 SELECT 'survey_questions' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM survey.survey_questions;
 
 
 SELECT 'survey_responses' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM survey.survey_responses;
 
 
 SELECT 'surveys_sent' as table_name
   , count(*) as num_rows
-  , MAX(sent_on) as date
+  , MAX(sent_on) as last_updated
 FROM survey.surveys_sent;
 
 
 SELECT 'thanked' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM survey.thanked;
 
 
 SELECT 'unsubscribed' as table_name
   , count(*) as num_rows
-  , NULL as date
+  , NULL as last_updated
 FROM survey.unsubscribed;
 
 
 SELECT 'facebook_page_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.facebook_page_data;
 
 
 SELECT 'facebook_post_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.facebook_post_data;
 
 
 SELECT 'instagram_metrics_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.instagram_metrics_data;
 
 
 SELECT 'instagram_user_detail' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.instagram_user_detail;
 
 
 SELECT 'twitter_followers_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.twitter_followers_data;
 
 
 SELECT 'twitter_tweets_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.twitter_tweets_data;
 
 
 SELECT 'youtube_metrics_data' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.youtube_metrics_data;
 
 
 SELECT 'youtube_video_details' as table_name
   , count(*) as num_rows
-  , MAX(updated_at) as date
+  , MAX(updated_at) as last_updated
 FROM unicorn_social_media.youtube_video_details;
