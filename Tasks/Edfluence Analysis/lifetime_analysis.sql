196ccb357e8d8870235c2e3d3f9ef24d91b7f552
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-04-10 16:52:03 -0700

    breakdown by membership type

--- Tasks/Edfluence Analysis/lifetime_analysis.sql
+++ Tasks/Edfluence Analysis/lifetime_analysis.sql
@@ -1,6 +1,6 @@
 
 /*
-  Total Buyers
+  Total Buyers -- Distinct CUSTID for that day
   Total Transactions
   Total Gross Sales - cumulative
   Total Refunds - cumulative
@@ -14,15 +14,28 @@
 
 SELECT
   A.date,
-  A.num_success,
-  B.num_canceled,
-  A.total_buyers,
+  A.num_success, -- transaction
+  B.num_canceled, -- transaction
+  A.total_buyers, -- customer
+  B.total_refunds, -- customer
   A.num_success + B.num_canceled as total_transactions,
   -- TODO: running total of members (needs to account for refunds)
   A.`Inner Circle Success`,
+  A.`Inner Circle Gross`,
   A.`Inner Circle JHA Success`,
+  A.`Inner Circle JHA Gross`,
   A.`Roadmap Success`,
+  A.`Roadmap Gross`,
   A.`Roadmap JHA Success`,
+  A.`Roadmap JHA Gross`,
+  B.`Inner Circle Refunded`,
+  B.`Inner Circle Refund Amount`,
+  B.`Inner Circle JHA Refunded`,
+  B.`Inner Circle JHA Refund Amount`,
+  B.`Roadmap Refunded`,
+  B.`Roadmap Refund Amount`,
+  B.`Roadmap JHA Refunded`,
+  B.`Roadmap JHA Refund Amount`,
   A.total_gross_USD,
   B.total_refunds_USD,
   A.total_gross_USD - B.total_refunds_USD AS net_sales_USD
@@ -48,6 +61,22 @@ FROM
       END AS "Roadmap JHA Success",
       COUNT(distinct user_id) as total_buyers,
       COUNT(distinct id) as num_success,
+      CASE
+        WHEN membership_id = 1 THEN sum(total)
+        else 0
+      END AS "Inner Circle Gross",
+      CASE
+        WHEN membership_id = 2 THEN sum(total)
+        else 0
+      END AS "Inner Circle JHA Gross",
+      CASE
+        WHEN membership_id = 3 THEN sum(total)
+        else 0
+      END AS "Roadmap Gross",
+      CASE
+        WHEN membership_id = 4 THEN sum(total)
+        else 0
+      END AS "Roadmap JHA Gross",
       SUM(total) as total_gross_USD
     FROM wp_pmpro_membership_orders
     WHERE status like 'success'
@@ -75,13 +104,31 @@ FROM
       END AS "Roadmap JHA Refunded",
       COUNT(DISTINCT user_id) AS total_refunds,
       COUNT(distinct id) as num_canceled,
+      CASE
+        WHEN membership_id = 1 THEN sum(total)
+        else 0
+      END AS "Inner Circle Refund Amount",
+      CASE
+        WHEN membership_id = 2 THEN sum(total)
+        else 0
+      END AS "Inner Circle JHA Refund Amount",
+      CASE
+        WHEN membership_id = 3 THEN sum(total)
+        else 0
+      END AS "Roadmap Refund Amount",
+      CASE
+        WHEN membership_id = 4 THEN sum(total)
+        else 0
+      END AS "Roadmap JHA Refund Amount",
       SUM(total) as total_refunds_USD
     FROM wp_pmpro_membership_orders
     WHERE status like 'cancelled'
-    group by date(timestamp)
+    group by date(timestamp), membership_id
   ) as B
   ON A.date = B.date
 ;
+
+
 
 SELECT
 #   COUNT(DISTINCT A.user_id) AS num_active
