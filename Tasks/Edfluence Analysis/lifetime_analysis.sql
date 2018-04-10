0a30cc2506e8528368501efd1a93f64705a287bd
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-04-10 14:07:01 -0700

    reorg scratch

--- Tasks/Edfluence Analysis/lifetime_analysis.sql
+++ Tasks/Edfluence Analysis/lifetime_analysis.sql
@@ -1,108 +1,20 @@
-SELECT * FROM wp_pmpro_membership_orders;
-
 
 /*
   Total Buyers
- */
-SELECT
-  COUNT(distinct user_id)
-FROM wp_pmpro_membership_orders
-WHERE status not like '%refunded%' -- refunded is a test entry
-;
-
-
-/*
   Total Transactions
- */
-SELECT
-  COUNT(*)
-FROM wp_pmpro_membership_orders
-WHERE status not like '%refunded%'  -- refunded is a test entry
-;
-
-
-/*
-  Total Gross Sales – daily and cumulative
- */
-
--- Group by date
-SELECT
-  date(timestamp) as date,
-  SUM(total) as gross_sales_USD
-FROM wp_pmpro_membership_orders
-GROUP BY date(timestamp)
-ORDER BY date(timestamp)
-;
-
-
-
-/*
-  Total Refunds – daily and cumulative
- */
-SELECT
-  date(timestamp) as date,
-  COUNT(*) as count
-FROM wp_pmpro_membership_orders
-WHERE status like 'cancelled'
-GROUP BY date(timestamp)
-ORDER BY date(timestamp)
-;
-
-/*
-  Total Refunds Amount
- */
-
-SELECT
-  date(timestamp) as date,
-  SUM(total) as total_refunds_USD
-FROM wp_pmpro_membership_orders
-WHERE status like 'cancelled'
-GROUP by date(timestamp)
-ORDER BY date(timestamp)
-;
-
-
-/*
-  Net Active – assuming LTD
- */
-
-
--- only has new data
-SELECT
-  date(timestamp) as date,
-  COUNT(DISTINCT user_id) AS active_members
-FROM wp_pmpro_membership_orders
-WHERE status like 'SUCCESS'
-GROUP BY date(timestamp)
-ORDER BY date(timestamp)
-;
-
-
-SELECT
-  status,
-  COUNT(*) as count
-FROM wp_pmpro_memberships_users
-GROUP BY status
-;
-
--- includes historical data
-SELECT
-  COUNT(DISTINCT user_id)
-FROM wp_pmpro_memberships_users
-WHERE status like 'active'
-;
-
-/*
+  Total Gross Sales - cumulative
+  Total Refunds - cumulative
+  Total Refund Amount - cumulative
   Net Sales – daily and cumulative
+  Net Active
 
   DOES NOT INCLUDE HISTORICAL DATA
  */
 
--- success minus refunds
-
 SELECT
   A.date,
   A.total_buyers,
+  -- TODO: Breakdown by Membership Type
   A.num_success,
   A.total_gross_USD,
   B.num_canceled,
--- Tasks/Edfluence Analysis/scratch.sql
+++ Tasks/Edfluence Analysis/scratch.sql
@@ -43,3 +43,108 @@ ORDER BY user_id
 SELECT *
 FROM wp_pmpro_membership_orders
 WHERE date(timestamp) = '2018-02-04'
+
+SELECT * FROM wp_pmpro_membership_orders;
+
+
+/*
+  Total Buyers
+ */
+SELECT
+  COUNT(distinct user_id)
+FROM wp_pmpro_membership_orders
+WHERE status not like '%refunded%' -- refunded is a test entry
+;
+
+
+/*
+  Total Transactions
+ */
+SELECT
+  COUNT(*)
+FROM wp_pmpro_membership_orders
+WHERE status not like '%refunded%'  -- refunded is a test entry
+;
+
+
+/*
+  Total Gross Sales – daily and cumulative
+ */
+
+-- Group by date
+SELECT
+  date(timestamp) as date,
+  SUM(total) as gross_sales_USD
+FROM wp_pmpro_membership_orders
+GROUP BY date(timestamp)
+ORDER BY date(timestamp)
+;
+
+
+
+/*
+  Total Refunds – daily and cumulative
+ */
+SELECT
+  date(timestamp) as date,
+  COUNT(*) as count
+FROM wp_pmpro_membership_orders
+WHERE status like 'cancelled'
+GROUP BY date(timestamp)
+ORDER BY date(timestamp)
+;
+
+/*
+  Total Refunds Amount
+ */
+
+SELECT
+  date(timestamp) as date,
+  SUM(total) as total_refunds_USD
+FROM wp_pmpro_membership_orders
+WHERE status like 'cancelled'
+GROUP by date(timestamp)
+ORDER BY date(timestamp)
+;
+
+
+/*
+  Net Active – assuming LTD
+ */
+
+
+-- only has new data
+SELECT
+  date(timestamp) as date,
+  COUNT(DISTINCT user_id) AS active_members
+FROM wp_pmpro_membership_orders
+WHERE status like 'SUCCESS'
+GROUP BY date(timestamp)
+ORDER BY date(timestamp)
+;
+
+
+SELECT
+  status,
+  COUNT(*) as count
+FROM wp_pmpro_memberships_users
+GROUP BY status
+;
+
+-- includes historical data
+SELECT
+  COUNT(DISTINCT user_id)
+FROM wp_pmpro_memberships_users
+WHERE status like 'active'
+;
+
+
+-- Get first success timestamp for all members from new membership orders data
+SELECT
+  user_id,
+  status,
+  min(timestamp) as first_success
+FROM wp_pmpro_membership_orders
+WHERE status like 'success'
+GROUP BY user_id, status
+;
