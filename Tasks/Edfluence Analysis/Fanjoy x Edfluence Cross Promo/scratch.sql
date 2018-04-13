2c56ff84dbbb720fa1896312a2f2a546da93b60c
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-04-13 15:10:03 -0700

    start edfluence cross promo

--- Tasks/Edfluence Analysis/Fanjoy x Edfluence Cross Promo/scratch.sql
+++ Tasks/Edfluence Analysis/Fanjoy x Edfluence Cross Promo/scratch.sql
@@ -1,1 +1,28 @@
+
+-- Get all active users
+SELECT
+  user_id
+FROM
+  wp_pmpro_memberships_users
+WHERE status = 'active'
+;
+
+SELECT * FROM wp_users;
+
+-- Get emails for active users
+select
+  id as userID,
+  user_email as email,
+  membership_id as membership_level
+from wp_users
+join
+  (
+    SELECT distinct
+      user_id,
+      membership_id
+    FROM
+      wp_pmpro_memberships_users
+    WHERE status = 'active'
+  ) AS A
+ON wp_users.id = A.user_id
 ;
