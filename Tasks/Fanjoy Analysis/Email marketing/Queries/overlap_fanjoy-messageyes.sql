66590e4c2865db0d57220fa496681de718e0f41e
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-04-16 11:45:56 -0700

    overlap phone logic

---
+++ Tasks/Fanjoy Analysis/Email marketing/Queries/overlap_fanjoy-messageyes.sql
@@ -0,0 +1,47 @@
+select count(distinct(order_number))
+from fanjoy_lineitems_data
+WHERE price > 0
+;
+
+
+
+
+-- Get Fanjoy and messageyes Overlap
+-- Get Fanjoy customers that have placed orders
+SELECT DISTINCT
+--   customer_id,
+  regexp_replace(phone, '[^0-9]+', '') as phone
+--   lower(email) as email,
+--   sum(sales) as total_spent
+--   COUNT(distinct lower(email))
+--   lower(email) as email
+
+FROM
+  (
+    SELECT DISTINCT
+      customer_id,
+      customer_email as email,
+      order_number
+    FROM fanjoy_orders_data
+    WHERE
+      customer_email not ilike '%fanjoy.co%'
+      AND phone is not NULL
+  ) as A
+  JOIN
+  (
+    SELECT
+      order_number,
+      sum(price) as sales
+    FROM fanjoy_lineitems_data
+    GROUP BY order_number
+  ) as B
+  ON a.order_number = b.order_number
+  JOIN
+  (
+    SELECT DISTINCT
+      id,
+      phone
+    FROM fanjoy_customers_data
+  ) AS C
+  on a.customer_id = c.id
+
