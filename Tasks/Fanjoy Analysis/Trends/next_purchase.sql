c344def936d70bf82052ce9e6f06cc877c06de69
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-04-16 17:18:06 -0700

    finished jake time of order dist

--- Tasks/Fanjoy Analysis/Trends/next_purchase.sql
+++ Tasks/Fanjoy Analysis/Trends/next_purchase.sql
@@ -189,8 +189,11 @@ ORDER BY orders_count
 
 
 
-
--- Jake Paul, only
+----------------------------------------------------------------
+/*
+  Jake Paul, only
+ */
+----------------------------------------------------------------
 
 SELECT
 --   newest_purch.customer_id,
@@ -199,7 +202,7 @@ SELECT
 --   prev_purch_date as previous,
   newest_purch.orders_count,
   COUNT(DISTINCT newest_purch.customer_id) as num_cust, -- Sample size for average
-  AVG(EXTRACT(DAY FROM newest_purch_date-prev_purch_date)) as avg_diff_days
+  avg(EXTRACT(DAY FROM newest_purch_date-prev_purch_date)) as avg_diff_days
 FROM (
   SELECT *
   FROM (
@@ -249,4 +252,79 @@ JOIN (
   ON newest_purch.customer_id = prev_purch.customer_id
 GROUP BY newest_purch.orders_count
 ORDER BY orders_count
+;
+
+
+SELECT
+  CASE
+    when orders_count = 1 then '1'
+    when orders_count = 2 then '2'
+    when orders_count = 3 then '3'
+    when orders_count = 4 then '4'
+    else '5+'
+  END AS orders_count,
+  avg_diff_days,
+  count(distinct customer_id) as num_cust
+FROM (
+  SELECT
+    newest_purch.orders_count                             AS orders_count,
+    newest_purch.customer_id,
+    EXTRACT(DAY FROM newest_purch_date - prev_purch_date) AS avg_diff_days
+  FROM (
+         SELECT *
+         FROM (
+                SELECT
+                  A.customer_id,
+                  B.num_orders                   AS orders_count,
+                  A.order_number,
+                  A.created_at                      newest_purch_date,
+                  rank()
+                  OVER (
+                    PARTITION BY A.customer_id
+                    ORDER BY A.created_at DESC ) AS rank
+                FROM fod_jakepaul A
+                  JOIN (
+                         SELECT DISTINCT
+                           f2.customer_id,
+                           count(DISTINCT f2.order_number) AS num_orders
+                         FROM fod_jakepaul f2
+                         GROUP BY f2.customer_id
+                       ) AS B
+                    ON A.customer_id = B.customer_id
+                WHERE A.customer_id > 0 AND A.customer_id IS NOT NULL
+              ) t
+         WHERE rank = 1 AND orders_count > 1
+         ORDER BY customer_id
+       ) newest_purch
+    JOIN (
+           SELECT *
+           FROM (
+                  SELECT
+                    A.customer_id,
+                    B.num_orders                   AS orders_count,
+                    A.order_number,
+                    A.created_at                      prev_purch_date,
+                    rank()
+                    OVER (
+                      PARTITION BY A.customer_id
+                      ORDER BY A.created_at DESC ) AS rank
+                  FROM fod_jakepaul A
+                    JOIN (
+                           SELECT DISTINCT
+                             f2.customer_id,
+                             count(DISTINCT f2.order_number) AS num_orders
+                           FROM fod_jakepaul f2
+                           GROUP BY f2.customer_id
+                         ) AS B
+                      ON A.customer_id = B.customer_id
+                  WHERE A.customer_id > 0 AND A.customer_id IS NOT NULL
+                ) t
+           WHERE rank = 2 AND orders_count > 1
+           ORDER BY customer_id
+         ) prev_purch
+      ON newest_purch.customer_id = prev_purch.customer_id
+  ORDER BY orders_count
+) t2
+GROUP BY 1,2
+order by 1 asc, 3 desc
 ;
--- Tasks/Fanjoy Analysis/Trends/quarterly_growth.sql
+++ Tasks/Fanjoy Analysis/Trends/quarterly_growth.sql
@@ -134,6 +134,236 @@ CREATE VIEW kevin_ip.qtr_sales_jake AS
         ON A.customer_ID = C.id
     GROUP BY 1, 2, 3, 4
     ORDER BY year, qtr
-)
+);
 
 
+
+-----------------------------------------------------------------
+/*
+  ALL OF FANJOY
+ */
+-----------------------------------------------------------------
+
+-- Q1 2018 sales
+SELECT
+  country,
+  city,
+  total_sales
+FROM qtr_sales_fanjoy
+WHERE year = 2018 and qtr = 1
+;
+
+-- Q4 2017 sales
+SELECT
+  country,
+  city,
+  total_sales
+FROM qtr_sales_fanjoy
+WHERE year = 2017 and qtr = 4
+;
+
+
+-- join Q1 '18 with Q4 '17
+SELECT
+  A.country,
+  A.city,
+  E.total_sales as sales_Q1_17,
+  D.total_sales as sales_Q2_17,
+  C.total_sales as sales_Q3_17,
+  B.total_sales as sales_Q4_17,
+  A.total_sales as sales_Q1_18
+FROM (
+  -- Q1 2018 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_fanjoy
+  WHERE year = 2018 and qtr = 1
+) A
+JOIN (
+  -- Q4 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_fanjoy
+  WHERE year = 2017 and qtr = 4
+) B
+  ON A.country = B.country
+  AND A.city = B.city
+JOIN (
+  -- Q3 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_fanjoy
+  WHERE year = 2017 and qtr = 3
+) C
+  ON A.country = C.country
+  AND A.city = C.city
+JOIN (
+  -- Q2 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_fanjoy
+  WHERE year = 2017 and qtr = 2
+) D
+  ON A.country = D.country
+  AND A.city = D.city
+JOIN (
+  -- Q1 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_fanjoy
+  WHERE year = 2017 and qtr = 1
+) E
+  ON A.country = E.country
+  AND A.city = E.city
+;
+
+
+-----------------------------------------------------------------
+/*
+  TEAM 10
+ */
+-----------------------------------------------------------------
+
+-- join Q1 '18 with Q4 '17
+SELECT
+  A.country,
+  A.city,
+  E.total_sales as sales_Q1_17,
+  D.total_sales as sales_Q2_17,
+  C.total_sales as sales_Q3_17,
+  B.total_sales as sales_Q4_17,
+  A.total_sales as sales_Q1_18
+FROM (
+  -- Q1 2018 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_team10
+  WHERE year = 2018 and qtr = 1
+) A
+JOIN (
+  -- Q4 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_team10
+  WHERE year = 2017 and qtr = 4
+) B
+  ON A.country = B.country
+  AND A.city = B.city
+JOIN (
+  -- Q3 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_team10
+  WHERE year = 2017 and qtr = 3
+) C
+  ON A.country = C.country
+  AND A.city = C.city
+JOIN (
+  -- Q2 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_team10
+  WHERE year = 2017 and qtr = 2
+) D
+  ON A.country = D.country
+  AND A.city = D.city
+JOIN (
+  -- Q1 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_team10
+  WHERE year = 2017 and qtr = 1
+) E
+  ON A.country = E.country
+  AND A.city = E.city
+;
+
+-----------------------------------------------------------------
+/*
+  JAKE PAUL, ONLY
+ */
+-----------------------------------------------------------------
+
+-- join Q1 '18 with Q4 '17
+SELECT
+  A.country,
+  A.city,
+  E.total_sales as sales_Q1_17,
+  D.total_sales as sales_Q2_17,
+  C.total_sales as sales_Q3_17,
+  B.total_sales as sales_Q4_17,
+  A.total_sales as sales_Q1_18
+FROM (
+  -- Q1 2018 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_jake
+  WHERE year = 2018 and qtr = 1
+) A
+JOIN (
+  -- Q4 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_jake
+  WHERE year = 2017 and qtr = 4
+) B
+  ON A.country = B.country
+  AND A.city = B.city
+JOIN (
+  -- Q3 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_jake
+  WHERE year = 2017 and qtr = 3
+) C
+  ON A.country = C.country
+  AND A.city = C.city
+JOIN (
+  -- Q2 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_jake
+  WHERE year = 2017 and qtr = 2
+) D
+  ON A.country = D.country
+  AND A.city = D.city
+JOIN (
+  -- Q1 2017 sales
+  SELECT
+    country,
+    city,
+    total_sales
+  FROM qtr_sales_jake
+  WHERE year = 2017 and qtr = 1
+) E
+  ON A.country = E.country
+  AND A.city = E.city
+;
