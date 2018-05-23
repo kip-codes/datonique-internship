d7492e9d1b3861739221adfaff179949fcaaa71e
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-05-23 11:38:54 -0700

    finished script for KLF

--- Tasks/KLF Analysis/Queries/KLF Lead Source Report/import_klf.py
+++ Tasks/KLF Analysis/Queries/KLF Lead Source Report/import_klf.py
@@ -7,38 +7,21 @@
 
 
 import datetime, time, os, sys, logging
+import boto3, psycopg2, botocore.exceptions
+from botocore.client import Config
+
 
 today = datetime.datetime.now()
-todayfn = str(today.month) + '-' + str(today.day) + '-' + str(today.year)
+yday = today - datetime.timedelta(days=1)
+yesterdayfn = str(yday.month) + '-' + str(yday.day) + '-' + str(yday.year)
 
+def get_S3_Key():
+    """Acquire the newest file within the target S3 subdirectory."""
 
+    ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
+    ACCESS_SECRET_KEY = os.environ['ACCESS_SECRET_KEY']
 
-def uploadToS3(credentials=None):
-    import boto3
-    from botocore.client import Config
-
-    ACCESS_KEY_ID=''
-    ACCESS_SECRET_KEY=''
-
-    print("Beginning upload to default S3 bucket...\n")
-
-    if credentials:
-        with open(credentials, 'r') as infile:
-            lines = []
-            for l in infile:
-                lines.append(l)
-            if len(lines) == 2:
-                ACCESS_KEY_ID = lines[0].strip()
-                ACCESS_SECRET_KEY = lines[1].strip()
-            else:
-                print("Improper formatting for AWS credential file. Proceeding to manual input.")
-                ACCESS_KEY_ID = input("Enter your AWS Access Key:\t")
-                ACCESS_SECRET_KEY = input("Enter your AWS Secret Access Key:\t")
-    else:
-        ACCESS_KEY_ID = input("Enter your AWS Access Key:\t")
-        ACCESS_SECRET_KEY = input("Enter your AWS Secret Access Key:\t")
-
-    # S3 Connect
+    # Create an S3 object
     s3 = boto3.resource(
         's3',
         aws_access_key_id=ACCESS_KEY_ID,
@@ -47,49 +30,91 @@ def uploadToS3(credentials=None):
     )
 
     # This is the name of the bucket on S3.
-    BUCKET_NAME = 'kevin-ip'
+    BUCKET = 'kevin-ip'
 
-    # Get name of .csv file(s) in directory
-    onlyfiles = [f for f in os.listdir('Reports/') if not f.startswith('.') and os.path.isfile(os.path.join('Reports/', f))]
+    # This is the subdirectory path inside the S3 Bucket, including the name of the file you want.
+    get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
 
-    if not onlyfiles:  # empty directory
-        print("There are no files in the Reports directory to be uploaded.")
-        return
+    objs = s3.list_objects_v2(Bucket=BUCKET)['Contents']
+    last_added = [obj['Key'] for obj in sorted(objs, key=get_last_modified)][-1]
 
-    print(onlyfiles)
+    KEY = 'klf/' + last_added
+    try:
+        s3.Object(BUCKET, KEY).load()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            # The object does not exist.
+            print("The object under " + KEY + " does not exist.")
+            quit()
+        else:
+            # Something else has gone wrong.
+            raise
+    else:
+        # The object does exist.
+        print("Attempt to load S3 object failed with no exception raised.")
+        quit()
 
-    objCount = 0
-    for n,f in enumerate(onlyfiles):  # only 1 expected, but may take more
-        # This is the subdirectory path inside the S3 Bucket, ending in the desired file name for S3
-        KEY = 'klf/' + f
-
-        # This is the local path to the file(s) you want to upload.
-        FILE_NAME = 'Reports/' + f
-        try:
-            data = open(FILE_NAME, 'rb')  # check if file exists
-        except:
-            print("WARNING: A valid file for " + FILE_NAME + " was not found. Exiting...")
-            time.sleep(1)
-            return
-
-        data = open(FILE_NAME, 'rb')
-
-        # CSV Upload
-        s3.Bucket(BUCKET_NAME).put_object(Key=KEY, Body=data, ACL='authenticated-read')
-        print("{} has been successfully uploaded.".format(f))
-        objCount += 1
-
-    print("\n" + str(objCount) + " files uploaded.")
-    print("All nodes have been successfully uploaded to the bucket '" + BUCKET_NAME + "'.")
+    return [BUCKET, KEY]
 
 
+def load_psql_query(s3_array):
+    print("Importing S3 file into Redshift...")
 
-def main():
-    logging.basicConfig(filename=__name__ + str(time.time()) + '-debug.log', level=logging.DEBUG)
+    schema = 'klf'
+
+    # REDSHIFT LOAD
+    if s3_array:
+        table = 'leads_source_report'
+        delete = None
+        copy = """
+                copy {schematable}
+                from 's3://{bucket}/{key}'
+                credentials 'aws_access_key_id={aws_key};aws_secret_access_key={aws_secret}'
+                CSV
+                IGNOREHEADER 1
+                DELIMITER ','
+                ACCEPTINVCHARS AS '?'
+                NULL as 'NULL'
+                REGION AS 'ap-south-1';
+                """.format(
+            schematable='{}.{}'.format(schema, table),
+            bucket= s3_array[0],
+            key= s3_array[1],
+            aws_key=os.environ['ACCESS_KEY_ID'],
+            aws_secret=os.environ['ACCESS_SECRET_KEY']
+            )
+        # Execute formatted query
+        s3_to_redshift_write(s3_array[1], copy, delete)
+    else:
+        print("ERROR:\tCOPY to Redshift failed.\n")
 
 
-    uploadToS3()
+def s3_to_redshift_write(key, copy, delete = None):
+    user = os.environ['REDSHIFT_USER']
+    password = os.environ['REDSHIFT_PASSWORD']
+    host = os.environ['REDSHIFT_HOST']
+    database = os.environ['REDSHIFT_DATABASE']
+    port = os.environ['REDSHIFT_PORT']
+    db = psycopg2.connect(
+        database=database,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    cur = db.cursor()
+    print("Connection established to database.")
+
+    if delete:
+        cur.execute(delete)
+    cur.execute(copy)
+    db.commit()
+    cur.close()
+    db.close()
+
+    print("\tCOPY command for {} was successful.\n".format(key))
 
 
-if __name__ == '__main__':
-    uploadToS3(sys.argv[1])
+def lambda_handler(event=None, context=None):
+    # logging.basicConfig(filename=__name__ + str(time.time()) + '-debug.log', level=logging.DEBUG)
+    load_psql_query(get_S3_Key())
