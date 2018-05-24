09369d3234d74a5a353b9822b26230a6f8918175
Kevin Ip - Macbook '13 <kevin.ip.j@gmail.com>
2018-05-24 09:56:54 -0700

    fixed KLF script for S3 Load

--- Tasks/KLF Analysis/Queries/KLF Lead Source Report/import_klf.py
+++ Tasks/KLF Analysis/Queries/KLF Lead Source Report/import_klf.py
@@ -6,53 +6,29 @@
 # For inquiries about the file please contact the author.
 
 
-import datetime, time, os, sys, logging
-import boto3, psycopg2, botocore.exceptions
-from botocore.client import Config
+import os, boto3, psycopg2, botocore.exceptions
 
-
-today = datetime.datetime.now()
-yday = today - datetime.timedelta(days=1)
-yesterdayfn = str(yday.month) + '-' + str(yday.day) + '-' + str(yday.year)
 
 def get_S3_Key():
     """Acquire the newest file within the target S3 subdirectory."""
 
-    ACCESS_KEY_ID = os.environ['ACCESS_KEY_ID']
-    ACCESS_SECRET_KEY = os.environ['ACCESS_SECRET_KEY']
-
     # Create an S3 object
-    s3 = boto3.resource(
-        's3',
-        aws_access_key_id=ACCESS_KEY_ID,
-        aws_secret_access_key=ACCESS_SECRET_KEY,
-        config=Config(signature_version='s3v4')
-    )
+    s3 = boto3.client('s3')
 
     # This is the name of the bucket on S3.
     BUCKET = 'kevin-ip'
 
+    # This is the subdirectory and file headers within the bucket you wish to limit the search to.
+    PREFIX = 'klf/Leads'
+
     # This is the subdirectory path inside the S3 Bucket, including the name of the file you want.
     get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
 
-    objs = s3.list_objects_v2(Bucket=BUCKET)['Contents']
+    objs = s3.list_objects_v2(Bucket=BUCKET, Prefix=PREFIX)['Contents']
     last_added = [obj['Key'] for obj in sorted(objs, key=get_last_modified)][-1]
 
-    KEY = 'klf/' + last_added
-    try:
-        s3.Object(BUCKET, KEY).load()
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            # The object does not exist.
-            print("The object under " + KEY + " does not exist.")
-            quit()
-        else:
-            # Something else has gone wrong.
-            raise
-    else:
-        # The object does exist.
-        print("Attempt to load S3 object failed with no exception raised.")
-        quit()
+    # Set the key
+    KEY = last_added
 
     return [BUCKET, KEY]
 
@@ -75,7 +51,8 @@ def load_psql_query(s3_array):
                 DELIMITER ','
                 ACCEPTINVCHARS AS '?'
                 NULL as 'NULL'
-                REGION AS 'ap-south-1';
+                REGION AS 'ap-south-1'
+                DATEFORMAT AS 'mm/dd/yy';
                 """.format(
             schematable='{}.{}'.format(schema, table),
             bucket= s3_array[0],
@@ -116,5 +93,14 @@ def s3_to_redshift_write(key, copy, delete = None):
 
 
 def lambda_handler(event=None, context=None):
-    # logging.basicConfig(filename=__name__ + str(time.time()) + '-debug.log', level=logging.DEBUG)
-    load_psql_query(get_S3_Key())
+    """Get the latest file from S3 and COPY into Redshift."""
+
+    s3array = get_S3_Key()
+
+    print(s3array)
+
+    load_psql_query(s3array)
+
+
+if __name__ == '__main__':
+    lambda_handler()
